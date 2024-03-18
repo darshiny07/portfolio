6b964d823f3651154fd970a7068f6a33f99d1353
darshiny07 <133087658+darshiny07@users.noreply.github.com>
2024-03-18 21:05:55 +0530

    Add files via upload

--- Home.jsx
+++ Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Home.css'
+import profile from './profile.webp'
 function Home() {
   return (
     <div className='Home_main'>
 <div className='Home_name'>
     <h1>Hai , I'm Priyadarshiny</h1>
     <h2>Front-End Developer</h2>
+</div>
+
+<div className='profile'>
+    <img src={profile} alt="myphoto" className='profile_img' />
 </div>
     </div>
   )
