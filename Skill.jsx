5791bb30b4a02d46fb3ac20fa8ba82666a583def
darshiny07 <133087658+darshiny07@users.noreply.github.com>
2024-03-21 17:49:35 +0530

    Add files via upload

--- Skill.jsx
+++ Skill.jsx
@@ -1,89 +1,74 @@
 import React from 'react'
 import { Element } from 'react-scroll'
-import skill from './skills.avif'
 import './Skill.css'
 import  { LinearProgress } from '@mui/material';
 
 function Skill() {
   return (
-   <Element className='skill_main' name='Skill'>
-    
-          <div className='skill_img'>
-            <img src={skill} alt="skill" width={'300px'} height={"285px"} />
+   <Element  name='Skill'>
+    <div className="skill_heading">
+        <h2>SKILLS</h2>
+    </div>
+ 
+<div className='full_Container'>
+    <div className="skillset_front">
+        <h2>FRONT-END </h2>
+        <div className="skill_item">
+            <h5>HTML 90%</h5>
+            <div className="progress_bar">
+                <LinearProgress variant='determinate' value={90} color='success'/>
             </div>
-
-            <div className='skill_text'>
-          <div className='Skill_header'>
-            <h2>SKILLS</h2>
-          </div>
-          
-                <h3>Front-End </h3>
-            <div className='skillContainer_skillSet'>
-                <h5>HTML 90%</h5>
-                <div className='skillSet_slider1'>
-               <LinearProgress variant='determinate' value={90} color='primary'/>
-                </div>
-            </div>
-
-
-            <div className='skillContainer_skillSet'>
-                <h5>CSS 80%</h5>
-                <div className='skillSet_slider2'>
-               <LinearProgress variant='determinate' value={80} color='primary' />
-                </div>
-            </div>
-
-            <div className='skillContainer_skillSet' >
-                <h5>JavaScript 75%</h5>
-                <div className='skillSet_slider3'>
-               <LinearProgress variant='determinate' value={75} color='primary'/>
-                </div>
-            </div>
-
-            <div className='skillContainer_skillSet' >
-                <h5>React Js 70%</h5>
-                <div className='skillSet_slider4'>
-               <LinearProgress variant='determinate' value={70} color='primary'/>
-                </div>
-            </div>
-
-         
-
         </div>
 
-{/* back end skill set */}
-        <div className='skill_text2'>
-           
-                <h3>Back-End </h3>
-            <div className='skillContainer_skillSet_bEnd'>
-                <h5>Node Js 50%</h5>
-                <div className='skillSet_slider1'>
-               <LinearProgress variant='determinate' value={50} color='primary'/>
-                </div>
+        <div className="skill_item">
+            <h5>CSS 80%</h5>
+            <div className="progress_bar">
+                <LinearProgress variant='determinate' value={80} color='secondary' />
             </div>
-
-
-            <div className='skillContainer_skillSet_bEnd'>
-                <h5>Mongo DB 70%</h5>
-                <div className='skillSet_slider2'>
-               <LinearProgress variant='determinate' value={70} color='primary'/>
-                </div>
-            </div>
-
-            <div className='skillContainer_skillSet_bEnd' >
-                <h5>My SQL 65%</h5>
-                <div className='skillSet_slider3'>
-               <LinearProgress variant='determinate' value={65} color='primary'/>
-                </div>
-            </div>
-
-           
         </div>
-         
-    
+
+        <div className="skill_item">
+            <h5>JavaScript 75%</h5>
+            <div className="progress_bar">
+                <LinearProgress variant='determinate' value={75} color='warning'/>
+            </div>
+        </div>
+
+        <div className="skill_item">
+            <h5>React Js 70%</h5>
+            <div className="progress_bar">
+                <LinearProgress variant='determinate' value={70} color='primary'/>
+            </div>
+        </div>
+    </div>
+
+    {/* back end skill set */}
+    <div className="skillset_Back">
+        <h2>BACK-END </h2>
+        <div className="skill_item">
+            <h5>Node Js 50%</h5>
+            <div className="progress_bar">
+                <LinearProgress variant='determinate' value={50} color='warning'/>
+            </div>
+        </div>
+
+        <div className="skill_item">
+            <h5>Mongo DB 70%</h5>
+            <div className="progress_bar">
+                <LinearProgress variant='determinate' value={70} color='secondary'/>
+            </div>
+        </div>
+
+        <div className="skill_item">
+            <h5>My SQL 65%</h5>
+            <div className="progress_bar">
+                <LinearProgress variant='determinate' value={65} color='primary'/>
+            </div>
+        </div>
+    </div>
+    </div>
    </Element>
   )
 }
 
 export default Skill
-
