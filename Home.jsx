8e7e3887186c6a84efa8ea59e6b34ba928a0e157
darshiny07 <133087658+darshiny07@users.noreply.github.com>
2024-03-21 21:19:45 +0530

    Add files via upload

--- Home.jsx
+++ Home.jsx
@@ -7,6 +7,9 @@ function Home() {
 <div className='Home_name'>
     <h1>Hai , I'm Priyadarshiny</h1>
     <h2>Front-End Developer</h2>
+    <p>
+
+"Passionate front-end developer with a hunger for innovation and a drive to explore new technologies. Committed to delivering cutting-edge solutions while continuously expanding my skill set through dedicated learning and exploration. Ready to tackle challenges head-on and contribute to the ever-evolving world of web development."</p>
 </div>
 
 <div className='profile'>
